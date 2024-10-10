aa4efece82f38444894c4e4251cc916d99a6899b
arcanum-technologies-pk <arcanum.technologies.pk@gmail.com>
2024-10-10 20:37:59 +0500

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -98,18 +98,54 @@ TUN needs to be enabled before running this installer."
 fi
 
 new_client_dns () {
-    echo "Automatically selecting DNS server: Current system resolvers"
+    echo "Select a DNS server for the client:"
+    echo "   1) Current system resolvers"
+    echo "   2) Google"
+    echo "   3) 1.1.1.1"
+    echo "   4) OpenDNS"
+    echo "   5) Quad9"
+    echo "   6) AdGuard"
 
-    # Locate the proper resolv.conf
-    # Needed for systems running systemd-resolved
-    if grep '^nameserver' "/etc/resolv.conf" | grep -qv '127.0.0.53' ; then
-        resolv_conf="/etc/resolv.conf"
-    else
-        resolv_conf="/run/systemd/resolve/resolv.conf"
-    fi
+    # Automatically select option 3
+    dns="3"
 
-    # Extract nameservers and provide them in the required format
-    dns=$(grep -v '^#\|^;' "$resolv_conf" | grep '^nameserver' | grep -v '127.0.0.53' | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | xargs | sed -e 's/ /, /g')
+    # Validate the selection
+    until [[ -z "$dns" || "$dns" =~ ^[1-6]$ ]]; do
+        echo "$dns: invalid selection."
+        read -p "DNS server [1]: " dns
+    done
+
+    # DNS selection handling
+    case "$dns" in
+        1|"")
+            # Locate the proper resolv.conf
+            if grep '^nameserver' "/etc/resolv.conf" | grep -qv '127.0.0.53' ; then
+                resolv_conf="/etc/resolv.conf"
+            else
+                resolv_conf="/run/systemd/resolve/resolv.conf"
+            fi
+            # Extract nameservers and provide them in the required format
+            dns=$(grep -v '^#\|^;' "$resolv_conf" | grep '^nameserver' | grep -v '127.0.0.53' | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | xargs | sed -e 's/ /, /g')
+            ;;
+        2)
+            dns="8.8.8.8, 8.8.4.4"
+            ;;
+        3)
+            dns="1.1.1.1, 1.0.0.1"
+            ;;
+        4)
+            dns="208.67.222.222, 208.67.220.220"
+            ;;
+        5)
+            dns="9.9.9.9, 149.112.112.112"
+            ;;
+        6)
+            dns="94.140.14.14, 94.140.15.15"
+            ;;
+    esac
+
+    # Optionally, you can output the selected DNS servers
+    echo "Selected DNS: $dns"
 }
 
 new_client_setup () {
