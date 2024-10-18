88ae4e2bde43ba6c254c1f8013b3ae3bf1fe0a80
arcanum-technologies-pk <arcanum.technologies.pk@gmail.com>
2024-10-18 18:26:14 +0500

    Update wireguard-remove.sh

--- wireguard-remove.sh
+++ wireguard-remove.sh
@@ -512,7 +512,7 @@ case "$option" in
 			done
 			client=$(grep '^# BEGIN_PEER' /etc/wireguard/wg0.conf | cut -d ' ' -f 3 | sed -n "$client_number"p)
 			echo
-			read -p "Confirm $client removal? [y/N]: " remove
+			read -p "Confirm $client removal? [y/N]: y"
 			until [[ "$remove" =~ ^[yYnN]*$ ]]; do
 				echo "$remove: invalid selection."
 				read -p "Confirm $client removal? [y/N]: " remove
